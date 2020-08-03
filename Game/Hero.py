473793768308db243b9e52fdaa454dc966de5857
Kawdin <kawan.inf@gmail.com>
2020-08-03 04:22:02 -0300

    Crew

--- Game/Hero.py
+++ Game/Hero.py
@@ -1,48 +1,63 @@
 import pygame
+from spritesheet import SpriteSheet
+
 
 class Hero(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
-        self.sprites = [pygame.image.load('data/hero_walk1.png'),
-                        pygame.image.load('data/hero_walk2.png')]
+        #self.sprites = [pygame.image.load('data/hero_walk1.png'),
+        #                pygame.image.load('data/hero_walk2.png')]
+        sprite_sheet = SpriteSheet('data/spritesheet.png')
+        self.sprites = {'HeroRun':[sprite_sheet.get_sprite(0, 9, 9, 9),
+                                   sprite_sheet.get_sprite(10, 9, 9, 9),
+                                   sprite_sheet.get_sprite(20, 9, 9, 9),(45,45)],
+                        'HeroGun':[sprite_sheet.get_sprite(32, 22, 11, 9),
+                                   sprite_sheet.get_sprite(44, 22, 11, 9),
+                                   sprite_sheet.get_sprite(56, 22, 11, 9),(55, 45)]}
+
+        self.animations = {'Gun': False, 'GunPerma': False}
+
         self.sprite_count = 1
+        self.key_down = []
+        self.sprite_anim = 'HeroRun'
         self.rotation = 0
         self.idle = 5
-        self.image = pygame.image.load('data/hero_idle.png')
-        self.image = pygame.transform.scale(self.image, [44, 44])
-        self.rect = pygame.Rect(300, 300, 7, 7)
+        self.image = sprite_sheet.get_sprite(0, 9, 9, 9)
+        self.image = pygame.transform.scale(self.image, [45, 45])
+        self.rect = pygame.Rect(300, 300, 44, 44)
 
     def update(self):
-        if(int(self.sprite_count) >= len(self.sprites)) :
+        if(int(self.sprite_count) >= len(self.sprites[self.sprite_anim])-1):
             self.sprite_count = 0
-        self.image = self.sprites[int(self.sprite_count)]
-        self.image = pygame.transform.scale(self.image, [44, 44])
+        self.image = self.sprites[self.sprite_anim][int(self.sprite_count)]
+        self.image = pygame.transform.scale(self.image, self.sprites[self.sprite_anim][-1])
 
         self.idle -= 0.5
         if self.idle <= 0:
             self.idle = 0
-            self.image = pygame.image.load('data/hero_idle.png')
-            self.image = pygame.transform.scale(self.image, [44, 44])
+            self.image = self.sprites[self.sprite_anim][0]
+            self.image = pygame.transform.scale(self.image, self.sprites[self.sprite_anim][-1])
 
         if self.rotation:
             self.image = pygame.transform.flip(self.image, True, False)
 
         keys = pygame.key.get_pressed()
+        #Movimento
         if(keys[pygame.K_w]):
             self.sprite_count += 0.05
             self.idle = 5
-            self.rect.y -= 5
+            self.rect.y -= 3
         if(keys[pygame.K_s]):
             self.sprite_count += 0.05
             self.idle = 5
-            self.rect.y += 5
+            self.rect.y += 3
         if(keys[pygame.K_a]):
             self.sprite_count += 0.05
             self.rotation = True
             self.idle = 5
-            self.rect.x -= 5
+            self.rect.x -= 3
         if(keys[pygame.K_d]):
             self.sprite_count += 0.05
             self.rotation = False
             self.idle = 5
-            self.rect.x += 5
+            self.rect.x += 3
--- Game/Main.py
+++ Game/Main.py
@@ -1,26 +1,57 @@
 import pygame
 from Hero import Hero
+from Items import Gun
 
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        self.tela = pygame.display.set_mode([600,600])
+        self.fps_clock = pygame.time.Clock()
+        self.GAMELOOP = True
 
-pygame.init()
+    def new(self):
+        self.items_group = pygame.sprite.Group()
+        self.hero_group = pygame.sprite.Group()
+        self.Hero = Hero(self.hero_group)
+        Gun(self.items_group)
 
-tela = pygame.display.set_mode([600,600])
+    def run(self):
+        self.new()
+        while self.GAMELOOP:
+            self.fps_clock.tick(60)
+            self.events()
+            self.draw()
+            self.update()
+            self.colisions()
 
-hero_group = pygame.sprite.Group()
-Hero = Hero(hero_group)
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.GAMELOOP = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    if self.Hero.animations['Gun'] and self.Hero.animations['GunPerma']:
+                        self.Hero.sprite_anim = 'HeroGun'
+                    else:
+                        self.Hero.sprite_anim = 'HeroRun'
+                    self.Hero.animations['Gun'] = not self.Hero.animations['Gun']
 
-fps_clock = pygame.time.Clock()
+    def draw(self):
+        self.tela.fill([68, 52, 191])
+        self.items_group.draw(self.tela)
+        self.hero_group.draw(self.tela)
 
-GAMELOOP = True
-while GAMELOOP:
-    fps_clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            GAMELOOP = False
+    def update(self):
+        self.items_group.update()
+        self.hero_group.update()
+        pygame.display.update()
 
-    tela.fill([68, 52, 191])
+    def colisions(self):
+        for item in pygame.sprite.spritecollide(self.Hero, self.items_group, True):
+            self.Hero.animations[item] = item.Pickup()[1]
+            self.Hero.animations[item.perma_item] = item.Pickup()[1]
+            self.Hero.sprite_anim = item.Pickup()[0]
 
-    hero_group.draw(tela)
-    hero_group.update()
-
-    pygame.display.update()
+game = Game()
+if game.GAMELOOP:
+    game.run()
